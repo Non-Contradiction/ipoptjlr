6fa2637b62fc7af922a145119e0060fffecb0db6
Non-Contradiction <lch34677@gmail.com>
2017-08-20 10:28:51 -0400

    Try to recover from the error by Pkg.build.

--- R/IPOPT.R
+++ R/IPOPT.R
@@ -162,6 +162,10 @@ IPOPT <- function(x,
 setup <- function() {
     .Ipopt$julia <- JuliaCall::julia_setup()
     .Ipopt$julia$install_package_if_needed("Ipopt")
-    .Ipopt$julia$using("Ipopt")
+    tryCatch(.Ipopt$julia$using("Ipopt"),
+             error = {
+                 system("julia -e 'Pkg.build(\"Ipopt\")'")
+                 .Ipopt$julia$using("Ipopt")
+             })
     .Ipopt$julia$source(system.file("julia/Ipopt2.jl", package = "ipoptjlr"))
 }
